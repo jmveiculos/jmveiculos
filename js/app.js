41dd60554e66fd214ef7b525a7d37f3b15a74ef6
teles <josetelesmaciel@gmail.com>
2015-09-10 19:33:36 -0300

    buscando fotos dos carros

--- js/app.js
+++ js/app.js
@@ -4,5 +4,9 @@ angular.module('jmapp').controller('indexCtrl', function ($scope, RevendaModel, 
   $scope.getUrls = function(){
   	jm.buscar_url_carros_da_revenda();
   };
+
+  $scope.getCarros = function(){
+  	jm.buscar_detalhes_lista_carros();
+  };
   
 });
--- js/revendamodel.js
+++ js/revendamodel.js
@@ -5,18 +5,18 @@ angular.module('revendamodel').factory('RevendaModel', function(RevendaApi, Carr
 	var jm = {
 		buscar_informacoes_da_revenda: false, 
 		buscando_informacoes_do_carro: false, 
+		buscando_informacoes_da_lista_de_carros: false, 
 		buscando_urls_carros: false, 
 		informacoes_revenda: null,
 		carros_url: new Array(),
 		informacoes_carro: null,
 		encontrou_carros_url: false,
+		carros: new Array(),
 	};
 
 	jm.buscar_informacoes_da_revenda = function(){
 		var deferred = $q.defer();
-
 		jm.buscar_informacoes_da_revenda = true;
-
 		RevendaApi.get_info().then(function(resposta){
 			jm.informacoes_revenda = resposta.data.query.results.revenda;
 			deferred.resolve(resposta);		
@@ -43,6 +43,22 @@ angular.module('revendamodel').factory('RevendaModel', function(RevendaApi, Carr
 				jm.buscando_urls_carros = false;
 		});
 	};
+	
+	jm.buscar_detalhes_lista_carros = function(){
+		jm.buscando_informacoes_da_lista_de_carros = true;
+		var deferred = $q.defer();
+
+		for (var i=0;i< jm.carros_url.length; i++) {
+			CarroApi.get_info(jm.carros_url[i]).then(function(resultado){
+				jm.carros.push(resultado.data.query.results.carro);
+			}).finally(function(){
+				jm.buscando_informacoes_da_lista_de_carros = false;				
+			});			
+		}
+		return deferred.promise;
+	};
+
+
 
 	return jm;
 });
